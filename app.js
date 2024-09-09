45f090df9b5da591bf3aabb1aa882d78c3e2edc1
alihasnain110 <alihasnain.0335@gmail.com>
2024-09-09 21:15:43 +0500

    Class Work JavaScript

---
+++ app.js
@@ -0,0 +1,97 @@
+
+
+    // INCREMENT / DECREMENT 
+
+// var num = 6;
+
+// console.log("before value of num: ",num);
+
+// post increment 
+
+// var result = ++num;
+
+// console.log("after value of num: ",result);
+
+
+
+
+// num++ , num-- after add or sub 
+
+// ++num , --num first add or sub 
+
+
+
+// questions 
+
+// var a = 4;
+// var b = 6;
+
+// var exp = a++ - b ;
+//  console.log("exp",exp);
+
+
+
+
+// var a = 3;
+// var b = 5;
+// var c = 2 ;
+//  var exp = a-- + --b + c - a + ++c - b-- - ++a - b++ + ++c + a++ - b + --c ;
+
+//             3 + 4   + 2 -  2 + 3   - 4    - 3 - 4   + 4  + 3 - 5 + 3
+
+//  console.log("exp",exp);
+
+
+
+// IF COMPLEX CALCULATION WE WILL USE BODMAS RULE :
+
+// BODMAS
+
+
+// BRACKET , ORDER DIVISION ,MULT , ADD ,SUB
+
+// var exp = 3*9 / (10 - 2)
+// console.log(exp)
+
+
+
+// Concatenating string 
+
+
+//  var message = "Thanks for your input!";
+//  alert(message);
+
+
+
+
+
+
+// PROMPT  use to take input from user 
+
+// var firstname = prompt("enter 1stname");
+// var lastname = prompt("enter 2nd name");
+ 
+// console.log(firstname,lastname);
+
+
+
+// var num1 = prompt("enter first no ");
+// var num2 = prompt("enter second no ");
+
+// console.log(num1 , num2);
+
+
+
+
+// Maths Expression 
+
+Chapter 5
+
+
+
+
+
+
+
+
+
